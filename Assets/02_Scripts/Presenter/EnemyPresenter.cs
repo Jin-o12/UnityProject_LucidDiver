a39d99c6eedea86e239077877d199e56066dac76
Jin-o12 <https://github.com/Jin-o12>
2026-07-03 14:45:53 +0900

    feat: 적 'DarkSpirit'(임시)의 움직임 애니메이션 연결
    - 적의 이동 애니메이션 및 바라보는 방향에 따른 좌우 이미지 반전 기능 추가
    - 플레이어의 접근 여부에 따라 공격 모션이 실행 되지만 공격 수행 이벤트가 반복적으로 실행되며 애니메이션이 잘못 출력되는 문제가 있음 (수정 예정)

---
+++ Assets/02_Scripts/Presenter/EnemyPresenter.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPresenter : MonoBehaviour
+{
+    [Header("필수 컴포넌트")]
+    [SerializeField] private EnemyMovement movement;     // 움직임
+    [SerializeField] private EnemyAnimator animator;     // 애니메이션
+    [SerializeField] private EnemyStatus status;         // 상태 관리
+
+    private void Awake()
+    {
+        // 필수 컴포넌트 초기화
+        movement = GetComponent<EnemyMovement>();
+        animator = GetComponentInChildren<EnemyAnimator>();
+        status = GetComponent<EnemyStatus>();
+        if (movement == null || animator == null || status == null)
+        {
+            enabled = false;
+            Debug.LogError("EnemyPresenter: required components are missing.");
+            return;
+        }
+
+
+    }
+
+    private void OnEnable()
+    {
+        // 이벤트 구독
+        movement.OnWalkEvent += animator.PlayWalk;
+        movement.OnAttackEvent += animator.PlayAttack;
+        movement.OnDeathEvent += animator.PlayDeath;
+        movement.OnLookDirEvent += animator.PlayLookDir;
+
+        animator.OnGetAttack += movement.CheckAndApplyDamage;
+    }
+
+    private void OnDisable()
+    {
+        // 구독 해제
+        movement.OnWalkEvent -= animator.PlayWalk;
+        movement.OnAttackEvent -= PlayAttack;
+        movement.OnDeathEvent -= animator.PlayDeath;
+        movement.OnLookDirEvent -= animator.PlayLookDir;
+
+        animator.OnGetAttack -= movement.CheckAndApplyDamage;
+    }
+
+    private void PlayAttack()
+    {
+        // 이미 공격 중이면 공격 애니메이션을 더 실행하지 않음
+        if(status.nowState == EnemyStatus.EnemyState.Attack) return;
+        animator.PlayAttack();
+    }
+}
